0c8872aaee5ec7b3c73ec7c58f1941d15d44f43d
sunnyswag <hhq126152@gmail.com>
2022-07-19 19:37:43 +0800

    在构造函数里面 getWidth 会为 0

--- Android/app/src/main/java/com/example/oneclicktriple/view/CircleAnimView.kt
+++ Android/app/src/main/java/com/example/oneclicktriple/view/CircleAnimView.kt
@@ -3,14 +3,14 @@ package com.example.oneclicktriple.view
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import com.example.oneclicktriple.Utils
 
 class CircleAnimView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
 
     private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val arcRect = RectF((width / 2).toFloat() - RADIUS, (height / 2).toFloat() - RADIUS,
-        (width / 2).toFloat() + RADIUS, (height / 2).toFloat() + RADIUS)
+    private val arcRect : RectF
 
     init {
         mPaint.apply {
@@ -19,15 +19,19 @@ class CircleAnimView(context: Context, attrs: AttributeSet?) : AppCompatImageVie
             style = Paint.Style.STROKE
         }
 
+        arcRect = RectF((width / 2).toFloat() - RADIUS, (height / 2).toFloat() - RADIUS,
+            (width / 2).toFloat() + RADIUS, (height / 2).toFloat() + RADIUS)
+        Log.d("huiqing", arcRect.toString() + "width / 2: ${height / 2}, width / 2: ${height / 2}")
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        Log.d("huiqing", arcRect.toString() + "width / 2: ${height / 2}, width / 2: ${height / 2}")
         canvas?.drawArc(arcRect,
-            0F, 360F, false, mPaint)
+            0F, 360F, true, mPaint)
     }
 
     companion object{
-        val RADIUS = Utils.dp2px(13F) // 半径
+        val RADIUS = Utils.dp2px(15F) // 半径
     }
 }
