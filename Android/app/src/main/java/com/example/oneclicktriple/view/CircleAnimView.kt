98f3c9985a12ec6380d34b413fb8f8b04ece36d3
sunnyswag <hhq126152@gmail.com>
2022-07-19 19:12:26 +0800

    奇怪的弧形出现了

--- Android/app/src/main/java/com/example/oneclicktriple/view/CircleAnimView.kt
+++ Android/app/src/main/java/com/example/oneclicktriple/view/CircleAnimView.kt
@@ -1,25 +1,33 @@
 package com.example.oneclicktriple.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import com.example.oneclicktriple.Utils
 
 class CircleAnimView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
 
     private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val arcRect = RectF((width / 2).toFloat() - RADIUS, (height / 2).toFloat() - RADIUS,
+        (width / 2).toFloat() + RADIUS, (height / 2).toFloat() + RADIUS)
 
     init {
-        mPaint.color = Color.parseColor("#e17c99")
-        mPaint.strokeWidth = 8F
-        mPaint.style = Paint.Style.STROKE
+        mPaint.apply {
+            color = Color.parseColor("#e17c99")
+            strokeWidth = 8F
+            style = Paint.Style.STROKE
+        }
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawCircle((width / 2).toFloat(), (height / 2).toFloat(),
-            (width / 2).toFloat() - mPaint.strokeWidth, mPaint)
+        canvas?.drawArc(arcRect,
+            0F, 360F, false, mPaint)
+    }
+
+    companion object{
+        val RADIUS = Utils.dp2px(13F) // 半径
     }
 }
